ce4b1919bfe43d2390e6079f99b29307060d046b
pushpendre <pushpendre@jhu.edu>
2014-05-18 23:46:23 -0400

    Fix up parallel processing bugs, add sufficient try catch

--- src/knn_performance.m
+++ src/knn_performance.m
@@ -10,7 +10,17 @@ tic;
 perf=0;
 indices=crossvalind('Kfold', label, crossval_fold);
 perf=zeros(1, crossval_fold);
-matlabpool('OPEN', crossval_fold);
+try
+    % This is all crap for parallel processing which is buggy
+    disp('I am running on machine'); unix('hostname');
+    distcomp.feature( 'LocalUseMpiexec', false );
+    matlabpool('OPEN', crossval_fold);
+catch
+    %do nothing
+    % Basically the loop will not parallelize and would be
+    % slow. but whatever.
+    disp('Failure to parallelize on this machine');
+end
 parfor i=1:crossval_fold
     disp(sprintf('Starting Fold number %d', i));
     test=(indices==i);
@@ -21,7 +31,14 @@ parfor i=1:crossval_fold
 end
 acc=sum(perf)/length(label)*100;
 time_taken=toc;
-
+try
+    % Same here, matlab needs to be told to close the things.
+    % Doesn't do that automatically. when the process exits. And
+    % this thing itself can give error so put it in a try catch.
+    matlabpool close force local;
+catch
+end
+    
 % performance_func=@(idx, label, len) ...
 %     sum(arrayfun(@(i) sum(label(idx(i,:))==label(i))-1, 1:len))/ ...
 %     len;
