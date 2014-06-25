e408152aff718cb722b1a399b075671c8e278649
pushpendre <pushpendre@jhu.edu>
2014-06-25 17:52:38 -0400

    IncrementalSVD and its test work correctly. Now I will fix se_gcca and its test

--- src/incrementalSVD.m
+++ src/incrementalSVD.m
@@ -1,4 +1,4 @@
-function [Ut_new, St_new]=incrementalSVD(C, U_tilde, S_tilde, r)
+function [Ut_new, St_new, St_discarded, Ut_discarded]=incrementalSVD(C, U_tilde, S_tilde, r)
 [N, b]=size(C); %#ok<ASGLU>
 L = U_tilde'*C;
 H = C - U_tilde*L;
@@ -7,6 +7,8 @@ Q = [diag(S_tilde) L; zeros(b,r) W];
 [Ut_prime, St_prime, ~]=fastsvd(Q,b);
 St_new=St_prime(1:r);
 Ut_new=[U_tilde J] * Ut_prime(:, 1:r);
+St_discarded = St_prime(r+1:length(St_prime));
+Ut_discarded = [U_tilde J] * Ut_prime(:, r+1:size(Ut_prime, 2));
 end
 
 function [U, S, V]=fastsvd(M, b) %#ok<INUSD>
--- src/test_incrementalSVD.m
+++ src/test_incrementalSVD.m
@@ -1,14 +1,15 @@
 clc;
-N=10;
-randn('seed', 42);
-X = randn(2*N, N); % N is th enumber of data points. 20 = dim
-X1 = X;
-X = X'*inv(X*X'+1e-8*eye(length(X)))*X;
+% Uncomment this line when running test.
+clear; load /export/a14/prastog3/align_ar.mat ; ar = align_mat; load /export/a14/prastog3/align_cs.mat ; cs = align_mat; clear align_mat; 
+f=@(X) X'*inv((1e-8)*eye(size(X, 1)) + X*X')*X;
+N=10; X1 = ar(1:N, 1:2*N)'; X2 = cs(1:N, 1:3*N)'; X  = f(X1)+f(X2);
+% disp(sort(eig(X))');
+
 [U, S, ~]=svd(X, 'econ');
+fprintf(1, ['true singular ' repmat('%f ', 1, N) '\n'], diag(S)');
 disp(norm(X - U*S*U', 'fro'));
 S=diag(S)';
-X_orig=X;
-fprintf(1, '%f %f %f %f %f %f %f %f %f %f \n', S);
+
 r=5;
 ep=5;
 U_tilde1=zeros(N, r);
@@ -16,32 +17,38 @@ S_tilde1=zeros(1, r);
 U_tilde2=zeros(N, N);
 S_tilde2=zeros(1, N);
 increment=1;
-for j=1:increment:size(X,2);
-    C=X(:, j:min(N,j+increment-1));
-    [U_tilde1, S_tilde1]=incrementalSVD(...
-        C, U_tilde1, S_tilde1, r);
-    [U_tilde2, S_tilde2]=incrementalSVD(...
-        C, U_tilde2, S_tilde2, N);
-    disp(j);
-    fprintf(1, '%f %f %f %f %f \n', S_tilde1);
-    fprintf(1, '%f %f %f %f %f %f %f %f %f %f\n', S_tilde2);
-end
-rec1 = U_tilde1*diag(S_tilde1)*U_tilde1';
-rec2 = U_tilde2*diag(S_tilde2)*U_tilde2';
-G = U_tilde1'; % Because we have founf the eigen vectors and
-fprintf(1, 'G^T*G - I = %d\n', (norm(G*G'-eye(size(G,1)))));
-% G - U' X => G' = X'U
-% b = Ax => x = pinv(A)*b
-% U = pinv(X') * G' => U = pinv(X1)'*U_tilde1
-U = pinv(X1)'*U_tilde1;
-emb1 = U'*X1;
-emb2 = (pinv(X1)'*U_tilde2(:, 1:5))'*X1;
-for i=1:5
-    disp(corr([emb1(i,:)', emb2(i,:)']));
+for batch=1:1
+    fprintf(1, 'the batch is %d\n', batch);
+    for j=1:increment:size(X,2);
+        C=X(:, j:min(N,j+increment-1));
+        [U_tilde1, S_tilde1]=incrementalSVD(...
+            C, U_tilde1, S_tilde1, r);
+        fprintf(1, '%f %f %f %f %f \n', S_tilde1);
+        if batch == 1
+            [U_tilde2, S_tilde2]=incrementalSVD(...
+                C, U_tilde2, S_tilde2, N);
+            fprintf(1, '%f %f %f %f %f %f %f %f %f %f\n', S_tilde2);
+        end
+        % disp(j);
+        % if j > r
+        %     disp('do special now');
+        % end
+    end
+    rec1 = U_tilde1*diag(S_tilde1)*U_tilde1';
+    G = U_tilde1'; % Because we have founf the eigen vectors and
+    fprintf(1, 'test that G is orthogonal, G^T*G - I = %d\n', (norm(G*G'-eye(size(G,1)))));
 end
 
-disp(norm(rec1 - X, 'fro'));
-for i=1:2:N
-    disp(norm(U_tilde2(:,1:i)*diag(S_tilde2(1:i))*U_tilde2(:,1:i)'- X, 'fro'));
-end
-
+% Importantly we dont have to compute U_j over the bilingual
+% matrices.
+% But we do want to know whether U_tilde1 and U_tilde2 (the group
+% configuration vectors) are a similar subspace. and measure how
+% similar they are ?
+% U_tilde1 is a [N r] matrix.
+% U_tilde2 is a [N N] matrix
+% Basically we can simply show how close the eigen directions are
+% to the true eigen directions.
+disp('Cosine between calculated direction and true direction is');
+disp(arrayfun(@(i) U_tilde1(:,i)'*U(:,i), 1:5));
+disp('Cosine between calculated direction 1 and calculated 2 is');
+disp(arrayfun(@(i) U_tilde1(:,i)'*U_tilde2(:,i), 1:5));
